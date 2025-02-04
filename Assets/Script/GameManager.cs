c2acd7f0ae7e6cdfd0e9c1ac40e9750e1dbb4bfb
ooSJW <tjwjddn1927@gmail.com>
2025-02-04 14:33:16 +0900

    25-02-04

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
+using static PlayerPrefsManager;
 
 public enum SceneName
 {
@@ -23,15 +24,17 @@ public partial class GameManager : MonoBehaviour // Data Field
     // TODO : 인벤토리, 퀘스트, 캐릭터 정보
 
     public CsvLoadManager csvLoadManager;
-
+    public PlayerPrefsManager playerPrefsManager;
     SceneName currentScene = SceneName.Lobby;
 
     private List<ItemInfo> itemInfoList = new List<ItemInfo>();
 
+    public GameObject itemPrefab;
     // Lobby Popup UI List
     [Header("LobbyScene")]
     public GameObject inventoryPopup;
     public GameObject questPopup;
+    public RectTransform content;
 }
 public partial class GameManager : MonoBehaviour // Property
 {
@@ -46,9 +49,19 @@ public partial class GameManager : MonoBehaviour // Property
         if (!isActive)
         {
             // TODO : 아이템 정보 csv 파일에서 읽어오기
-            List<ItemInfo> itemInfoList = csvLoadManager.GetItemInfo();
             // TODO : ui 아이템 팝업 연동
             // TODO : playerPrefs에서 아이템 정보 가져오기, 해당 아이템 정보를 csv에서 찾아 세팅 ㄱㄱ
+            List<ItemInfo> itemInfoList = csvLoadManager.GetItemInfo();
+            HashSet<Item> inventoryHashSet = playerPrefsManager.GetItemListInfo();
+            foreach (Transform child in content)
+            {
+                Destroy(child);
+            }
+            foreach (Item item in inventoryHashSet)
+            {
+                GameObject itemObject = Instantiate(itemPrefab, content);
+                itemObject.transform.localScale = Vector3.one;
+            }
         }
         inventoryPopup.SetActive(!isActive);
     }
